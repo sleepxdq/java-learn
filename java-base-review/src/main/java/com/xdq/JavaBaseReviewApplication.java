0bbd4ebc67ab40840feeb3df9139af96c3f2dc4c
sleepxdq <xu_dongqiang@foxmail.com>
2017-08-27 16:19:18 +0800

    add new model: java-base-review

---
+++ java-base-review/src/main/java/com/xdq/JavaBaseReviewApplication.java
@@ -0,0 +1,12 @@
+package com.xdq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBaseReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBaseReviewApplication.class, args);
+	}
+}
